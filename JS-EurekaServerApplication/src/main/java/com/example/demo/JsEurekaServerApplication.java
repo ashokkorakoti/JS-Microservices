4c613f4d0b33d9e4650991580785c86208cd5885
Ashok korakoti <korakotiashok@gmail.com>
2021-09-20 17:56:23 +0530

    first commit

---
+++ JS-EurekaServerApplication/src/main/java/com/example/demo/JsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsEurekaServerApplication.class, args);
+	}
+
+}
